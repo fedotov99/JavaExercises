481a975246998de531de75dc5e7fbcb61be51596
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-07 22:56:10 +0300

    points added

--- src/com/company/points/MovablePoint.java
+++ src/com/company/points/MovablePoint.java
@@ -1,4 +1,77 @@
 package com.company.points;
 
+import java.util.Objects;
+
 public class MovablePoint extends Point2D {
+    float xSpeed = 0.0f;
+    float ySpeed = 0.0f;
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint() {
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        setXSpeed(xSpeed);
+        setYSpeed(ySpeed);
+    }
+
+    public float[] getSpeed() {
+        float[] result = new float[2];
+        result[0] = getXSpeed();
+        result[1] = getYSpeed();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", speed = (" + xSpeed + ", " + ySpeed + ")";
+    }
+
+    public MovablePoint move() {
+        this.x += xSpeed;
+        this.y += ySpeed;
+
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        MovablePoint that = (MovablePoint) o;
+        return Float.compare(that.xSpeed, xSpeed) == 0 &&
+                Float.compare(that.ySpeed, ySpeed) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), xSpeed, ySpeed);
+    }
 }
--- src/com/company/points/Point3D.java
+++ src/com/company/points/Point3D.java
@@ -1,4 +1,56 @@
 package com.company.points;
 
+import java.util.Objects;
+
 public class Point3D extends Point2D {
+    float z = 0.0f;
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public Point3D() {
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        setX(x);
+        setY(y);
+        setZ(z);
+    }
+
+    public float[] getXYZ(float x, float y, float z) {
+        float[] result = new float[3];
+        result[0] = getX();
+        result[1] = getY();
+        result[2] = getZ();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Point3D point3D = (Point3D) o;
+        return Float.compare(point3D.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), z);
+    }
 }
